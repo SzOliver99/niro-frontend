0dcc952656b6f972eb0c13893dae8e64d54b3a0b
Szvetnyik Oliver <oliver.szvetnyik@gmail.com>
2025-07-28 18:44:14 +0200

    Permission to links

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import { fetchIsAnyPermission } from '$lib/scripts/apis/user';
 	import {
 		ArrowLeftToLine,
 		ArrowRightFromLine,
@@ -8,25 +9,34 @@
 		User,
 		Users
 	} from 'lucide-svelte';
-	let { isMenuOpen = $bindable() } = $props();
+	import { onMount } from 'svelte';
+	let { isMenuOpen = $bindable(), data } = $props();
+
 	const links = [
 		{
 			href: '/employee-management',
 			title: 'Employees',
+			permissionRequired: true,
 			icon: Users
 		},
-		{ href: '/contacts', title: 'Contacts', icon: BookUser },
-		{ href: '', title: 'Home', icon: Calendar },
-		{ href: '', title: 'Home', icon: Calendar },
-		{ href: '', title: 'Home', icon: Calendar },
-		{ href: '', title: 'Home', icon: Calendar }
+		{ href: '/contacts', title: 'Contacts', permissionRequired: false, icon: BookUser },
+		{ href: '', title: 'Home', permissionRequired: false, icon: Calendar },
+		{ href: '', title: 'Home', permissionRequired: false, icon: Calendar },
+		{ href: '', title: 'Home', permissionRequired: false, icon: Calendar },
+		{ href: '', title: 'Home', permissionRequired: false, icon: Calendar }
 	];
+
+	let hasPermission = $state(false);
+	onMount(async () => {
+		let fetchPermission = await fetchIsAnyPermission(data.token);
+		hasPermission = fetchPermission.data;
+	});
 </script>
 
 <div
 	class="sidebar relative flex h-full flex-col bg-[#F6F6F6] p-2 shadow-2xl transition-all duration-200"
 	class:items-center={!isMenuOpen}
-	class:rounded-r-lg={!isMenuOpen}
+	class:rounded-r-lg={isMenuOpen}
 	class:closed={!isMenuOpen}
 >
 	{#if isMenuOpen}
@@ -37,7 +47,9 @@
 			</button>
 		</div>
 		<div class="ms-2 mt-3 flex">
-			<a href="/" class="text-2xl font-bold text-black hover:text-gold text-shadow-md duration-200">Niro</a>
+			<a href="/" class="text-2xl font-bold text-black duration-200 text-shadow-md hover:text-gold"
+				>Niro</a
+			>
 		</div>
 		<div class="mt-5 flex flex-col font-medium">
 			<a href="/profile" class="mb-6">Profile</a>
@@ -53,21 +65,23 @@
 			</button>
 		</div>
 		<div
-			class="flex h-full flex-col items-center justify-center gap-4 *:duration-200 *:hover:text-gold"
+			class="flex h-full flex-col items-center justify-center gap-4 *:duration-200 *:hover:scale-115 *:hover:text-gold"
 		>
 			<a href="/profile" class="mb-6" title="Profile">
 				<User
 					strokeWidth={2}
-					class="size-7 {$page.url.pathname === '/profile' ? 'scale-115 text-gold' : ''}"
+					class="size-7 {$page.url.pathname.includes('/profile') ? 'scale-115 text-gold' : ''}"
 				/>
 			</a>
 			{#each links as link}
-				<a href={link.href} title={link.title}>
-					<link.icon
-						strokeWidth={2}
-						class="size-6 {$page.url.pathname === link.href ? 'scale-120 text-gold' : ''}"
-					/>
-				</a>
+				{#if !link.permissionRequired || hasPermission}
+					<a href={link.href} title={link.title}>
+						<link.icon
+							strokeWidth={2}
+							class="size-6 {$page.url.pathname === link.href ? 'scale-120 text-gold' : ''}"
+						/>
+					</a>
+				{/if}
 			{/each}
 		</div>
 	{/if}
