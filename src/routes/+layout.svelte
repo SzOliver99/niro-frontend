f6c1a62d83c9d54e01d1b9453e6b8b820c522f27
Szvetnyik Oliver <oliver.szvetnyik@gmail.com>
2025-07-30 12:38:37 +0200

    Add search functionality and user profile link to header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { page } from '$app/stores';
 	import Sidebar from '$lib/components/Sidebar.svelte';
+	import { Search } from 'lucide-svelte';
 	import '../app.css';
 
 	let { children, data } = $props();
 
-	let isMenuOpen = $state(false);
+	let isMenuOpen = $state(true);
 </script>
 
 <div class="flex h-screen w-screen overflow-hidden bg-white">
@@ -14,9 +15,23 @@
 			<Sidebar bind:isMenuOpen {data} />
 		</div>
 	{/if}
+
 	<div class="m-2 flex-1 overflow-auto">
-		<header></header>
+		<header class="flex w-full items-center justify-between">
+			<div>
+				<button class="flex items-center rounded-md border px-3 py-1 text-black/30">
+					<Search class="me-3 size-4" />
+					<p class="me-5">Keresés...</p>
+				</button>
+			</div>
+			<div>
+				<a href="/profile">
+					<img src="https://avatar.iran.liara.run/public/boy" class="w-12" alt="" />
+				</a>
+			</div>
+		</header>
 		<main>
+			<!-- <img src={teszt} class="bg-black" alt="" /> -->
 			{@render children()}
 		</main>
 		<footer></footer>
