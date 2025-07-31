64cba26fbeade05107220005c070cefe794f3fcc
Oliver <rosszbikalo@gmail.com>
2025-07-31 16:21:29 +0200

    Refactor Sidebar component to improve layout and toggle functionality, enhancing user experience with conditional rendering for menu items.

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -62,154 +62,144 @@
 	class:rounded-r-lg={isMenuOpen}
 	class:closed={!isMenuOpen}
 >
+	<!-- {#if isMenuOpen} -->
+	<!-- OPENED SIDEBAR LAYOUT -->
 	{#if isMenuOpen}
-		<!-- OPENED SIDEBAR LAYOUT -->
 		<div class="absolute top-6 right-4">
 			<button onclick={() => (isMenuOpen = false)}>
 				<ArrowLeftToLine class="duration-200 hover:text-blue-600" />
 			</button>
 		</div>
-		<div class="ms-2 mt-3 flex">
-			<a
-				href="/"
-				class="text-2xl font-bold text-black duration-200 text-shadow-md hover:text-blue-600"
-				>Niro</a
-			>
-		</div>
-		<div class="flex h-full flex-col justify-center gap-8 text-sm text-nowrap">
-			<!-- SALES -->
-			<div>
-				<h3
-					class="mb-2 bg-gradient-to-r from-blue-600 to-pink bg-clip-text font-medium text-transparent uppercase"
-				>
-					{sidebarSections.sales.category}
-				</h3>
-
-				<ul class="flex flex-col text-start">
-					{#each sidebarSections.sales.items as link}
-						<li>
-							{#if !link.permissionRequired || hasPermission}
-								<a
-									href={link.href}
-									title={link.label}
-									class="group flex items-center gap-2 rounded-lg px-2 py-1 *:duration-200 {$page
-										.url.pathname === link.href
-										? 'bg-gray-50/60 text-blue-600'
-										: ''}"
-								>
-									<link.icon class="text-blue-600 group-hover:scale-115" />
-									<p class="group-hover:text-blue-600">{link.label}</p>
-								</a>
-							{/if}
-						</li>
-					{/each}
-				</ul>
-			</div>
-
-			<!-- TEAM MANAGEMENT -->
-			<div>
-				<h3
-					class="mb-2 bg-gradient-to-r from-blue-600 to-pink bg-clip-text font-medium text-transparent uppercase"
-				>
-					{sidebarSections.teamManagement.category}
-				</h3>
-
-				<ul class="flex flex-col text-start">
-					{#each sidebarSections.teamManagement.items as link}
-						<li>
-							{#if !link.permissionRequired || hasPermission}
-								<a
-									href={link.href}
-									title={link.label}
-									class="group flex items-center gap-2 rounded-lg px-2 py-1 *:duration-200 {$page
-										.url.pathname === link.href
-										? 'bg-gray-50/60 text-blue-600'
-										: ''}"
-								>
-									<link.icon class="text-blue-600 group-hover:scale-115" />
-									<p class="group-hover:text-blue-600">{link.label}</p>
-								</a>
-							{/if}
-						</li>
-					{/each}
-				</ul>
-			</div>
-			<!-- USEFUL LINKS -->
-			<div>
-				<h3
-					class="mb-2 bg-gradient-to-r from-blue-600 to-pink bg-clip-text font-medium text-transparent uppercase"
-				>
-					{sidebarSections.usefulLinks.category}
-				</h3>
-
-				<ul class="flex flex-col text-start">
-					{#each sidebarSections.usefulLinks.items as link}
-						<li>
-							{#if !link.permissionRequired || hasPermission}
-								<a
-									href={link.href}
-									title={link.label}
-									class="group flex items-center gap-2 rounded-lg px-2 py-1 *:duration-200 {$page
-										.url.pathname === link.href
-										? 'bg-gray-50/60 text-blue-600'
-										: ''}"
-								>
-									<link.icon class="text-blue-600 group-hover:scale-115" />
-									<p class="group-hover:text-blue-600">{link.label}</p>
-								</a>
-							{/if}
-						</li>
-					{/each}
-				</ul>
-			</div>
-		</div>
 	{:else}
-		<!-- CLOSED SIDEBAR LAYOUT -->
 		<div class="absolute top-6">
 			<button onclick={() => (isMenuOpen = true)}>
 				<ArrowRightFromLine class="duration-200 hover:text-blue-600" />
 			</button>
 		</div>
-		<div
-			class="flex h-full flex-col items-center justify-center gap-4 *:duration-200 *:hover:scale-115 *:hover:text-blue-600"
-		>
-			<!-- SALES -->
-			{#each sidebarSections.sales.items as link}
-				{#if !link.permissionRequired || hasPermission}
-					<a href={link.href} title={link.title}>
-						<link.icon
-							strokeWidth={2}
-							class="size-6 {$page.url.pathname === link.href ? 'scale-120 text-blue-600' : ''}"
-						/>
-					</a>
-				{/if}
-			{/each}
-
-			<!-- SALES -->
-			{#each sidebarSections.teamManagement.items as link}
-				{#if !link.permissionRequired || hasPermission}
-					<a href={link.href} title={link.title}>
-						<link.icon
-							strokeWidth={2}
-							class="size-6 {$page.url.pathname === link.href ? 'scale-120 text-blue-600' : ''}"
-						/>
-					</a>
-				{/if}
-			{/each}
-
-			<!-- SALES -->
-			{#each sidebarSections.usefulLinks.items as link}
-				{#if !link.permissionRequired || hasPermission}
-					<a href={link.href} title={link.title}>
-						<link.icon
-							strokeWidth={2}
-							class="size-6 {$page.url.pathname === link.href ? 'scale-120 text-blue-600' : ''}"
-						/>
-					</a>
-				{/if}
-			{/each}
-		</div>
 	{/if}
+	<div class="ms-2 mt-3 flex">
+		{#if isMenuOpen}
+			<a
+				href="/"
+				class="text-2xl font-bold text-black duration-200 text-shadow-md hover:text-blue-600"
+				>Niro</a
+			>
+		{/if}
+	</div>
+
+	<div class="flex h-full flex-col justify-center gap-8 text-sm text-nowrap">
+		<!-- SALES -->
+		<div>
+			<h3
+				class="mb-2 bg-gradient-to-r from-blue-600 to-pink bg-clip-text font-medium text-transparent uppercase"
+			>
+				{#if isMenuOpen}
+					{sidebarSections.sales.category}
+				{:else}
+					<p class="text-center">...</p>
+				{/if}
+			</h3>
+
+			<ul class="flex flex-col text-start">
+				{#each sidebarSections.sales.items as link}
+					<li>
+						{#if !link.permissionRequired || hasPermission}
+							<a
+								href={link.href}
+								title={link.label}
+								class="group flex items-center gap-2 rounded-lg px-2 py-1 *:duration-200 {$page.url
+									.pathname === link.href
+									? 'bg-gray-50/60 text-blue-600'
+									: ''}"
+							>
+								<link.icon class="shrink-0 text-blue-600 group-hover:scale-115" />
+								{#if isMenuOpen}
+									<p class="group-hover:text-blue-600">{link.label}</p>
+								{/if}
+							</a>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		</div>
+
+		<!-- TEAM MANAGEMENT -->
+		<div>
+			<h3
+				class="mb-2 bg-gradient-to-r from-blue-600 to-pink bg-clip-text font-medium text-transparent uppercase"
+			>
+				{#if isMenuOpen}
+					{sidebarSections.teamManagement.category}
+				{:else}
+					<p class="text-center">...</p>
+				{/if}
+			</h3>
+
+			<ul class="flex flex-col text-start">
+				{#each sidebarSections.teamManagement.items as link}
+					<li>
+						{#if !link.permissionRequired || hasPermission}
+							<a
+								href={link.href}
+								title={link.label}
+								class="group flex items-center gap-2 rounded-lg px-2 py-1 *:duration-200 {$page.url
+									.pathname === link.href
+									? 'bg-gray-50/60 text-blue-600'
+									: ''}"
+							>
+								<link.icon class="shrink-0 text-blue-600 group-hover:scale-115" />
+								{#if isMenuOpen}
+									<p class="group-hover:text-blue-600">{link.label}</p>
+								{/if}
+							</a>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<!-- USEFUL LINKS -->
+		<div>
+			<h3
+				class="mb-2 bg-gradient-to-r from-blue-600 to-pink bg-clip-text font-medium text-transparent uppercase"
+			>
+				{#if isMenuOpen}
+					{sidebarSections.usefulLinks.category}
+				{:else}
+					<p class="text-center">...</p>
+				{/if}
+			</h3>
+
+			<ul class="flex flex-col text-start">
+				{#each sidebarSections.usefulLinks.items as link}
+					<li>
+						{#if !link.permissionRequired || hasPermission}
+							<a
+								href={link.href}
+								title={link.label}
+								class="group flex items-center gap-2 rounded-lg px-2 py-1 *:duration-200 {$page.url
+									.pathname === link.href
+									? 'bg-gray-50/60 text-blue-600'
+									: ''}"
+							>
+								<link.icon class="shrink-0 text-blue-600 group-hover:scale-115" />
+								{#if isMenuOpen}
+									<p class="group-hover:text-blue-600">{link.label}</p>
+								{/if}
+							</a>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+	<!-- {:else} -->
+	<!-- CLOSED SIDEBAR LAYOUT -->
+	<!-- <div class="absolute top-6">
+			<button onclick={() => (isMenuOpen = true)}>
+				<ArrowRightFromLine class="duration-200 hover:text-blue-600" />
+			</button>
+		</div>
+	{/if} -->
 </div>
 
 <style>
