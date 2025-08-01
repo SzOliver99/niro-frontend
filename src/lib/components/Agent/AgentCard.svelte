45dbd74c635f13f0712a33986125e07f811788bb
Oliver <rosszbikalo@gmail.com>
2025-08-01 13:04:54 +0200

    Refactor AgentCard to utilize user_info for displaying agent details

--- src/lib/components/Agent/AgentCard.svelte
+++ src/lib/components/Agent/AgentCard.svelte
@@ -3,8 +3,10 @@
 
 	let { agent } = $props();
 
-	console.log(agent);
-	
+	const convertUserGroup = (group) =>
+		group === 'Leader' ? 'Hálózati igazgató' : group === 'Manager' ? 'Menedzser' : 'Üzletkötő';
+
+	$inspect(agent); // DEBUG
 </script>
 
 <div
@@ -12,10 +14,10 @@
 >
 	<div class="rounded-b-lg bg-gradient-to-t from-black p-3 text-white">
 		<div class="pb-3">
-			<h3 class="font-bold">{agent?.full_name}</h3>
-			<p>{agent?.group}</p>
-			<p>{agent?.email}</p>
-			<p>{agent?.phone_number}</p>
+			<h3 class="font-bold">{agent?.user_info.full_name}</h3>
+			<p>{convertUserGroup(agent?.user_group)}</p>
+			<p>{agent?.user_info.agent_code}</p>
+			<p>{agent?.user_info.phone_number}</p>
 		</div>
 		<button
 			class="flex w-full justify-between rounded-lg bg-blue-600 px-5 py-2 font-medium duration-200 hover:scale-105 hover:bg-blue-700"
