70d3591a9b021bfd3b80d295bc7fb5ab44d29830
Szvetnyik Oliver <oliver.szvetnyik@gmail.com>
2025-08-05 16:36:48 +0200

    Update profile layout to include a white background for the account information section, enhancing visual clarity.

--- src/routes/profile/+layout.svelte
+++ src/routes/profile/+layout.svelte
@@ -25,7 +25,7 @@
 			class="mx-auto my-5 flex flex-col overflow-hidden rounded-lg px-2 py-4 md:w-[80%] lg:w-[60%]"
 		>
 			<h1 class="mb-4 text-2xl font-bold">Account Information</h1>
-			<div class="rounded-lg shadow-lg ring ring-black/10">
+			<div class="rounded-lg shadow-lg ring ring-black/10 bg-white">
 				<div class="flex h-full justify-between border-b border-black/10 py-2 *:font-medium">
 					<div class="flex flex-row *:duration-200 *:hover:scale-105">
 						{#each list as item}
