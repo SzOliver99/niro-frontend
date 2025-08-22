2ef86f56a41e2fbe8888a2f6835d4c1c746dbc05
Oliver <rosszbikalo@gmail.com>
2025-08-22 20:06:37 +0200

    Remove autofetch

--- src/lib/scripts/queries/user.js
+++ src/lib/scripts/queries/user.js
@@ -1,45 +1,47 @@
-import { createMutation, createQuery, queryOptions, useQueryClient } from "@tanstack/svelte-query";
-import userApi from "../apis/user";
-import { Notification } from "$lib/stores/notifications";
+import { createMutation, createQuery, queryOptions, useQueryClient } from '@tanstack/svelte-query';
+import userApi from '../apis/user';
+import { Notification } from '$lib/stores/notifications';
 
-export const getUsersQuery = (user_token) => queryOptions({
-    queryKey: ['users', user_token],
-    queryFn: () => userApi().getAllUsers(user_token)
-});
+export const getUsersQuery = (user_token) =>
+	queryOptions({
+		queryKey: ['users', user_token],
+		queryFn: () => userApi().getAllUsers(user_token)
+	});
 
-export const getManagerGroupQuery = (user_token) => queryOptions({
-    queryKey: ['manager-group', user_token],
-    queryFn: () => userApi().getManagerGroup(user_token),
-    refetchInterval: 3000,
-    refetchIntervalInBackground: false
-})
+export const getManagerGroupQuery = (user_token) =>
+	queryOptions({
+		queryKey: ['manager-group', user_token],
+		queryFn: () => userApi().getManagerGroup(user_token)
+		// refetchInterval: 3000,
+		// refetchIntervalInBackground: false
+	});
 
-export const getManagersQuery = () => queryOptions({
-    queryKey: ['managers'],
-    queryFn: () => userApi().getManagers()
-})
+export const getManagersQuery = () =>
+	queryOptions({
+		queryKey: ['managers'],
+		queryFn: () => userApi().getManagers()
+	});
 
 export const useUpdateUsers = (user_token) => {
-    const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-    return createMutation({
-        mutationFn: async (user) => await userApi().modifyUserInfo(user_token, user),
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['users'] });
-            queryClient.invalidateQueries({ queryKey: ['manager-group'] });
-        }
-    });
+	return createMutation({
+		mutationFn: async (user) => await userApi().modifyUserInfo(user_token, user),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['users'] });
+			queryClient.invalidateQueries({ queryKey: ['manager-group'] });
+		}
+	});
 };
 
 export const useCreateUsers = (user_token) => {
-    const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-    return createMutation({
-        mutationFn: async (user) => await userApi().signUp(user, user_token),
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['users'] });
-            queryClient.invalidateQueries({ queryKey: ['manager-group'] });
-        }
-    });
+	return createMutation({
+		mutationFn: async (user) => await userApi().signUp(user, user_token),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['users'] });
+			queryClient.invalidateQueries({ queryKey: ['manager-group'] });
+		}
+	});
 };
-
