9ae8be37a401a971a87484474bb5b6a8221e8dc2
Szvetnyik Oliver <oliver.szvetnyik@gmail.com>
2025-08-27 13:49:47 +0200

    fix

--- src/routes/(sales)/leads/+page.svelte
+++ src/routes/(sales)/leads/+page.svelte
@@ -1,6 +1,19 @@
 <script>
 	import DataTable from '$lib/components/data/DataTable.svelte';
+	import userApi from '$lib/scripts/apis/user';
+	import { convertUserGroup } from '$lib/scripts/utils';
+	import { permissionsStore } from '$lib/stores/permissions';
+	import { createQuery } from '@tanstack/svelte-query';
 	import { Plus } from 'lucide-svelte';
+
+	let { data } = $props();
+
+	let sub_users = createQuery({
+		queryKey: ['sub_users', data.token],
+		queryFn: async () => await userApi().getUserSubUsers(data.token)
+	});
+
+	let selected_user = $state();
 
 	const columns = [
 		{ key: 'action', label: '#' },
@@ -29,7 +42,7 @@
 		closed: 'Lezárt'
 	};
 
-	const data = [
+	const user_data = [
 		{
 			id: 1,
 			name: 'Alice Johnson',
@@ -629,7 +642,22 @@
 
 <div class="p-4">
 	<div class="flex justify-between text-center">
-		<h1 class="mb-4 text-xl font-semibold">Címanyagok</h1>
+		<div class="flex items-center gap-4">
+			<h1 class="text-xl font-semibold">Címanyagok</h1>
+			<select
+				id="select_user"
+				name="select_user"
+				bind:value={selected_user}
+				disabled={$permissionsStore.userRole === 'Agent'}
+				class="mt-1 block w-full rounded-md px-3 py-2 ring-1 ring-black/10 duration-200 focus:ring-blue-600 focus:outline-none"
+			>
+				{#each $sub_users.data as user}
+					<option value={user.id}
+						>{user.info?.full_name} - {convertUserGroup(user.user_role)}</option
+					>
+				{/each}
+			</select>
+		</div>
 		<button
 			class="bg-gray me-4 flex items-center rounded-lg bg-blue-600 px-3 py-2 text-center text-nowrap text-white duration-200 hover:bg-blue-700"
 		>
@@ -637,5 +665,12 @@
 			<p>Címanyag hozzáadása</p>
 		</button>
 	</div>
-	<DataTable {data} {columns} searchable={true} filterable={true} sortable={true} pageSize={30} />
+	<DataTable
+		{user_data}
+		{columns}
+		searchable={true}
+		filterable={true}
+		sortable={true}
+		pageSize={30}
+	/>
 </div>
