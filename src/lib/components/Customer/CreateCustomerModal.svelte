b9bd8f67c22228ce03774a19782e168d52685ef3
Szvetnyik Oliver <oliver.szvetnyik@gmail.com>
2025-08-28 19:00:14 +0200

    modify: Remove formatting check to "Házszám"

--- src/lib/components/Customer/CreateCustomerModal.svelte
+++ src/lib/components/Customer/CreateCustomerModal.svelte
@@ -148,7 +148,6 @@
 						<input
 							name="house_number"
 							id="house_number"
-							oninput={() => formatNumberOnly(house_number)}
 							type="text"
 							class="mt-1 block w-full rounded-md px-3 py-2 ring-1 ring-black/10 duration-200 focus:ring-blue-600 focus:outline-none"
 							autocomplete="off"
