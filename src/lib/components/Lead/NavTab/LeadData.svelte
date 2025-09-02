2d09ceb2a788895c6e99220f5520ff73318a5f24
Szvetnyik Oliver <oliver.szvetnyik@gmail.com>
2025-09-02 12:03:57 +0200

    fix: Swap inquiry type and lead status fields in LeadData component for improved user experience

--- src/lib/components/Lead/NavTab/LeadData.svelte
+++ src/lib/components/Lead/NavTab/LeadData.svelte
@@ -34,31 +34,31 @@
 		</select>
 	</div>
 	<div class="flex flex-col text-start font-medium">
-		<label for="inquiry_type">Megkeresés jellege <span class="text-red-700">*</span></label>
-		<input
-			name="inquiry_type"
-			id="inquiry_type"
-			type="text"
-			bind:value={lead.inquiry_type}
+		<label for="lead_status">Státusz <span class="text-red-700">*</span></label>
+		<select
+			name="lead_status"
+			id="lead_status"
+			bind:value={lead.lead_status}
 			class="mt-1 block w-full rounded-md px-3 py-2 ring-1 ring-black/10 duration-200 focus:ring-blue-600 focus:outline-none"
 			autocomplete="off"
 			required
-		/>
+		>
+			<option value="">Válassz státust</option>
+			{#each Object.entries(statusTypes) as [key, value]}
+				<option value={key}>{value}</option>
+			{/each}
+		</select>
 	</div>
 </div>
 <div class="flex flex-col text-start font-medium">
-	<label for="lead_status">Státusz <span class="text-red-700">*</span></label>
-	<select
-		name="lead_status"
-		id="lead_status"
-		bind:value={lead.lead_status}
+	<label for="inquiry_type">Megkeresés jellege <span class="text-red-700">*</span></label>
+	<input
+		name="inquiry_type"
+		id="inquiry_type"
+		type="text"
+		bind:value={lead.inquiry_type}
 		class="mt-1 block w-full rounded-md px-3 py-2 ring-1 ring-black/10 duration-200 focus:ring-blue-600 focus:outline-none"
 		autocomplete="off"
 		required
-	>
-		<option value="">Válassz státust</option>
-		{#each Object.entries(statusTypes) as [key, value]}
-			<option value={key}>{value}</option>
-		{/each}
-	</select>
+	/>
 </div>
