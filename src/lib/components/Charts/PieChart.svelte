82f3881766b840a12bbb91c87fd1670190601656
Szvetnyik Oliver <oliver.szvetnyik@gmail.com>
2025-10-02 20:15:09 +0200

    fix: Update PieChart data handling to use correct accumulator in reduce function for accurate calculations

--- src/lib/components/Charts/PieChart.svelte
+++ src/lib/components/Charts/PieChart.svelte
@@ -9,10 +9,10 @@
 
 <div class="relative my-3">
 	<h1 class="ms-2 text-start text-lg italic">{title}</h1>
-	{#if data.datasets[0].values.reduce((i) => i + i) != 0}
+	{#if data.datasets[0].values.reduce((acc, curr) => acc + curr) != 0}
 		<Chart {data} {colors} {maxSlices} type="pie" height={300} {chartRef} />
 		<p class="text-center">
-			Összesen: {data.datasets[0].values.reduce((i, k) => i + k)}
+			Összesen: {data.datasets[0].values.reduce((acc, curr) => acc + curr)}
 		</p>
 	{:else}
 		<p class="mt-5 text-center">Nincs megjeleníthető adat</p>
